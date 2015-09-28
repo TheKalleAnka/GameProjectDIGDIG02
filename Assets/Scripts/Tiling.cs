3bc005b4d08e4661fdbded0205ad25d2040462a6
TheKalleAnka <marcus.bjors@hotmail.com>
2015-09-28 14:15:03 +0200

    Easy prototype

---
+++ Assets/Scripts/Tiling.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+[ExecuteInEditMode]
+[RequireComponent(typeof(SpriteRenderer))]
+public class Tiling : MonoBehaviour {
+
+    SpriteRenderer sr;
+
+	// Use this for initialization
+	void Start () {
+	    sr = GetComponent<SpriteRenderer>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+        sr.sharedMaterial.SetFloat("RepeatX", Mathf.Round(transform.localScale.x));
+        sr.sharedMaterial.SetFloat("RepeatY", Mathf.Round(transform.localScale.y));
+
+        Vector3 scale = transform.localScale;
+        scale.x = Mathf.Round(transform.localScale.x);
+        scale.y = Mathf.Round(transform.localScale.y);
+        transform.localScale = scale;
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Round(pos.x);
+        pos.y = Mathf.Round(pos.y);
+        transform.position = pos;
+	}
+}
