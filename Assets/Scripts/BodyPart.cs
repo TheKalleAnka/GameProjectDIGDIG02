7e06f4111a68ee327b5a6e80106aca52038d3da9
TheKalleAnka <marcus.bjors@hotmail.com>
2016-01-11 14:04:39 +0100

    2016-01-11
    Added rudimentary character clothes selection

---
+++ Assets/Scripts/BodyPart.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BodyPart : MonoBehaviour {
+
+    [SerializeField]
+    int activeSprite = 0;
+
+    [SerializeField]
+    Sprite[] possibleSprites;
+
+    private SpriteRenderer sRenderer;
+
+    void Start()
+    {
+        sRenderer = GetComponent<SpriteRenderer>();
+    }
+
+	void Update () {
+	
+	}
+
+    public void ChooseNextSprite()
+    {
+        activeSprite++;
+
+        if (activeSprite >= possibleSprites.Length)
+            activeSprite = 0;
+        
+        sRenderer.sprite = possibleSprites[activeSprite];
+    }
+
+    public void ChoosePreviousSprite()
+    {
+        activeSprite--;
+
+        if (activeSprite < 0)
+            activeSprite = possibleSprites.Length - 1;
+
+        sRenderer.sprite = possibleSprites[activeSprite];
+    }
+}
