d2b0312e1f277384be14abbe2e5a3ced3686bc63
FudgeLyfe <filip.flodwall@gmail.com>
2016-04-04 13:21:05 +0200

    5.6
    saving färdigt

--- Assets/Scripts/Saving.cs
+++ Assets/Scripts/Saving.cs
@@ -4,44 +4,60 @@ using System.Xml;
 
 public class Saving : MonoBehaviour {
 
-    static public int spriteId_1 = 5;
-    static string spriteID_1;
+    static public int headSprite = 1;
+    static public int legsSprite;
+    static public int torsoSprite;
+
+    static public int level;
+
+    static public int coins = 100;
 
     
 
-	// Use this for initialization
 	void Start () 
     {
         
 	}
 	
-	// Update is called once per frame
 	void Update () 
     {
         Save();
-        
+        Load();
 	}
 
     static void Save()
     {
-        spriteID_1 = spriteId_1.ToString();
 
         XmlDocument xmlDoc = new XmlDocument();
-        XmlNode rootNode = xmlDoc.CreateElement("Sprite-Id");
+        XmlNode rootNode = xmlDoc.CreateElement("Root");
         xmlDoc.AppendChild(rootNode);
 
-        XmlNode node1 = xmlDoc.CreateElement("Bodyparts");
-        rootNode.AppendChild(node1);
 
-        XmlNode nodeHead = xmlDoc.CreateElement("Head");
-        node1.AppendChild(nodeHead);
+        
 
-        XmlNode bodyparts = xmlDoc.CreateElement("Sprite-Id");
-        XmlAttribute spriteId = xmlDoc.CreateAttribute("Id");
-        spriteId.Value = spriteID_1;
-        bodyparts.Attributes.Append(spriteId);
-        nodeHead.AppendChild(bodyparts);
-        Debug.Log(spriteID_1);
+        XmlNode bodyparts = xmlDoc.CreateElement("Bodyparts");
+        XmlAttribute nodeHead = xmlDoc.CreateAttribute("HeadId");
+        XmlAttribute nodeTorso = xmlDoc.CreateAttribute("TorsoId");
+        XmlAttribute nodeLegs = xmlDoc.CreateAttribute("LegsId");
+        nodeLegs.Value = legsSprite.ToString();
+        nodeHead.Value = headSprite.ToString();
+        nodeTorso.Value = torsoSprite.ToString();
+        bodyparts.Attributes.Append(nodeLegs);
+        bodyparts.Attributes.Append(nodeTorso);
+        bodyparts.Attributes.Append(nodeHead);
+        rootNode.AppendChild(bodyparts);
+
+        XmlNode coins = xmlDoc.CreateElement("Coins");
+        XmlAttribute nodeCoins = xmlDoc.CreateAttribute("CoinsNum");
+        nodeCoins.Value = coins.ToString();
+        coins.Attributes.Append(nodeCoins);
+        rootNode.AppendChild(coins);
+
+        XmlNode level = xmlDoc.CreateElement("Level");
+        XmlAttribute nodeLevel = xmlDoc.CreateAttribute("LevelNum");
+        nodeLevel.Value = level.ToString();
+        level.Attributes.Append(nodeLevel);
+        rootNode.AppendChild(level);
 
         xmlDoc.Save("Save.xml");
     }
@@ -49,11 +65,33 @@ public class Saving : MonoBehaviour {
     {
         XmlDocument xmlDoc = new XmlDocument();
         xmlDoc.Load("Save.xml");
-        foreach (XmlNode xmlNode in xmlDoc.DocumentElement.ChildNodes[2].ChildNodes[0].ChildNodes)
+        XmlNodeList idNodes = xmlDoc.SelectNodes("//Root/Bodyparts");
+        XmlNodeList coinNodes = xmlDoc.SelectNodes("//Root/Coins");
+        XmlNodeList levelNodes = xmlDoc.SelectNodes("//Root/Level");
+
+        foreach (XmlNode node1 in idNodes)
         {
-            Debug.Log(xmlNode.Attributes["Id"].Value);
+            int head = int.Parse(node1.Attributes["HeadId"].Value);
+            int torso = int.Parse(node1.Attributes["TorsoId"].Value);
+            int legs = int.Parse(node1.Attributes["LegsId"].Value);
+
+            Debug.Log("Head: " + head);
+            Debug.Log("Torso: " + torso);
+            Debug.Log("Legs: " + legs);
+
         }
+        foreach (XmlNode node2 in coinNodes)
+        {
+            int coin = int.Parse(node2.Attributes["CoinsNum"].Value);
+
+            Debug.Log("Coins: " + coin);
+        }
+        foreach (XmlNode node3 in levelNodes)
+        {
+            int level = int.Parse(node3.Attributes["LevelNum"].Value);
+
+            Debug.Log("Level: " + level);
+        }
+        
     }
-
-
 }
